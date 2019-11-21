c25cb7cb3e9a00ab32934c2adea5bf0f05137811
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-11-21 05:39:36 -0600

    created firebase, added var config

---
+++ timelogic.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDFSAsscmiWzDBZieaHdLT9Byc9kxS9WtE",
+    authDomain: "train-timer-176d8.firebaseapp.com",
+    databaseURL: "https://train-timer-176d8.firebaseio.com",
+    projectId: "train-timer-176d8",
+    storageBucket: "train-timer-176d8.appspot.com",
+    messagingSenderId: "951292739243",
+    appId: "1:951292739243:web:0763e55c3d7809098c81ee"
+  };
