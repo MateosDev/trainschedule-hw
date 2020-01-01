b2894765806369e2615ca89c509e45c094bf7ead
MateosDev <35548023+MateosDev@users.noreply.github.com>
2020-01-01 06:35:54 -0600

    fixed it

--- timelogic.js
+++ timelogic.js
@@ -12,121 +12,86 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
+
+
 // 2. Add Trains Btn
-$("#add-train-btn").on("click", function () {
+$("#add-train-btn").on("click", function (event) {
   event.preventDefault();
 
-  // Input
-  var trainName = $("#train-name-input")
-    .val()
-    .trim();
+ 
+// USER INPUT 
+  
 
-  var trainDest = $("#dest-input")
-    .val()
-    .trim();
+  var trainName = $("#train-name-input").val().trim();
 
-  var trainFirst = moment(
-    $("#first-input")
-    .val()
-    .trim(),
-    "mm"
-  ).format("mm");
+  var trainDest = $("#dest-input").val().trim();
 
-  var trainFreq = $("#freq-input")
-    .val()
-    .trim();
+  var trainFirst = moment($("#first-input").val().trim(),"HH:mm").format("X");
+
+  var trainFreq = $("#freq-input").val().trim();
 
   // train data obj
   var newTrain = {
     name: trainName,
-    dest: trainDest,
-    first: trainFirst,
-    freq: trainFreq
+    destination: trainDest,
+    time: trainFirst,
+    frequency: trainFreq
+    
   };
 
   // Uploads train data to the database
-  database.ref("/trainData").push(newTrain);
+  database.ref().push(newTrain);
 
-  function printRow() {
-    //console.log("printing");
-    var tRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(destination),
-        $("<td class='frequency'>").text(frequency),
-        $("<td class='nextTime'>").text(nextArrival),
-        $("<td class='minAway'>").text(minutesAway)
-    );
-
-    $("tbody").append(tRow);
-}
-
-printRow();
-
-  // Logs everything to console
   console.log(newTrain.name);
-  console.log(newTrain.dest);
-  console.log(newTrain.freq);
-  console.log(newTrain.start);
-  // console.log(newTrain.nextArrival);
-  // console.log(newTrain.minutesAway);
+  console.log(newTrain.destination);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
 
-  alert("train successfully added");
-
-  // Clears all 
-  $("#train-name-input").val("");
-  $("#dest-input").val("");
-  $("#freq-input").val("");
-  $("#nextArrival-input").val("");
-});
-
-
+   // Clears all 
+   $("#train-name-input").val("");
+   $("#dest-input").val("");
+   $("#first-input").val("");
+   $("#freq-input").val("");
+ });
 
 // 3. adds train and row to DB with user input
-database.ref().on("child_added", function (childSnapshot) {
+database.ref().on("child_added", function(childSnapshot) {
   console.log(childSnapshot.val());
+  
 
-  // Store everything into a variable.
-  var trainName = childSnapshot.val().name;
-  var trainDest = childSnapshot.val().dest;
-  var trainFreq = childSnapshot.val().freq;
-  var trainFirst = childSnapshot.val().first;
-  // var trMA = childSnapshot.val();
-  // var trNA = childSnapshot.val().nextArrival;
-  // var trMA = childSnapshot.val().minutesAway;
+// Store everything into a variable.
+var trainName = childSnapshot.val().name;
+var trainDest = childSnapshot.val().destination;
+var trainFirst = childSnapshot.val().time;
+var trainFreq = childSnapshot.val().frequency;
 
-  // train Info
+  // Logs everything to console
   console.log(trainName);
   console.log(trainDest);
-  console.log(trainFreq);
   console.log(trainFirst);
+  console.log(trainFreq);
 
-  var firstTimeConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  var trainDiff = moment().diff(moment.unix(trainFirst), "minutes");
+  var trainRemainder = trainDiff % trainFreq;
+  var trainETA = trainFreq - trainRemainder;
+  var trainNext = moment().add(trainETA, "m").format("hh:mm A");
 
-  // math is fun. Math, math math. 
-  var tRemainder = diffTime % trainFreq;
-  //_______________________
-  var trMA = trFreq - tRemainder;
-  var trNA = moment()
-    .add(trMA, "minutes")
-    .format("HH:mm");
-  console.log(trNA);
-  console.log(trMA);
+var inputRow = $("<tr>").append(
+  $("<td>").text(trainName),
+  $("<td>").text(trainDest),
+  $("<td>").text(trainFreq),
+  $("<td>").text(trainNext),
+  $("<td>").text(trainETA)
+);
 
-  // Create the new row with i <3 jqueryyyyy
-  var newRow = $("<tr>").append(
-    $("<td>").text(trainName),
-    $("<td>").text(trainDest),
-    $("<td>").text(trainFreq),
-    $("<td>").text(trainNA),
-    $("<td>").text(trainMA),
-  );
+$("#train-table > tbody").append(inputRow);
 
-  // Append the new row to the table
-  $("#train-table > tbody").append(newRow);
+
+
+
+  
+
 });
-
-
 
 //Timeout Function - Keep at bottom
 // setTimeout(function () {
